810e74d77d6594d9e82cb04c3184493e8eb74d02
macbook <markussjoman1@gmail.com>
2023-03-03 11:00:35 +0100

    changed variables from local to class variables. Created methods for setOnclickListener and calculateBmi.

--- app/src/main/java/com/markofsuccess/bmicalculator/MainActivity.java
+++ app/src/main/java/com/markofsuccess/bmicalculator/MainActivity.java
@@ -3,12 +3,73 @@ package com.markofsuccess.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    //class variables/fields
+    private TextView resultText;
+    private Button calculateButton;
+    private RadioButton maleButton;
+    private RadioButton femaleButton;
+    private EditText feetEditText;
+    private EditText inchesEditText;
+    private EditText ageEditText;
+    private EditText weightEditText;
+
+    //onCreate is like the main Main Method
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        findViews();
+
+        setupButtonClickListener();
+
+
     }
+
+    private void findViews () {
+
+        //ctrl+alt + f för göra de till fields //eller refactor introduce fields.
+         resultText = findViewById(R.id.text_view_result);
+
+        maleButton = findViewById(R.id.radio_button_male);
+        femaleButton = findViewById(R.id.radio_button_female);
+
+
+        feetEditText = findViewById(R.id.edit_text_feet);
+        inchesEditText = findViewById(R.id.edit_text_inches);
+        ageEditText = findViewById(R.id.edit_text_age);
+        weightEditText = findViewById(R.id.edit_text_weight);
+
+       calculateButton = findViewById(R.id.button_calculate);
+    }
+    private void setupButtonClickListener() {
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculateBmi();
+
+            }
+        });
+    }
+
+    private void calculateBmi() {
+       String ageText = ageEditText.getText().toString();
+        String feetText = feetEditText.getText().toString();
+        String inchesText = inchesEditText.getText().toString();
+        String weightText = weightEditText.getText().toString();
+
+        resultText.setText("Age: " + ageText + ", Feet: " + feetText + ", Inches: " + inchesText+ ", Weight: " + weightText);
+    }
+
 }
